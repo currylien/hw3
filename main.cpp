6cf55b2c393d72533129d24a3506aa270ee8003d
currylien <79573460+currylien@users.noreply.github.com>
2021-05-01 06:36:03 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -18,15 +18,15 @@
 #include "stm32l475e_iot01_accelero.h"
 
 constexpr int kTensorArenaSize = 60 * 1024;
-uint8_t tensor_arena[kTensorArenaSize];            // Lab8 ML
+uint8_t tensor_arena[kTensorArenaSize];            
 
 WiFiInterface *wifi;
 InterruptIn btn(USER_BUTTON);
 BufferedSerial pc(USBTX, USBRX);
-//DigitalIn btn(USER_BUTTON);
 uLCD_4DGL uLCD(D1, D0, D2);
 DigitalOut myled1(LED1);
 DigitalOut myled2(LED2);
+DigitalOut myled3(LED3);
 void UI_gesture(Arguments *in, Reply *out);
 void angle_detection(Arguments *in, Reply *out);
 void gesture(void);
@@ -59,30 +59,40 @@ EventQueue queue;
 EventQueue queue2;
 Thread th;
 Thread th2;
-//EventQueue queue(32 * EVENTS_EVENT_SIZE);
 
 void messageArrived(MQTT::MessageData& md) {
-    MQTT::Message &message = md.message;
-    char msg[300];
-    sprintf(msg, "Message arrived: QoS%d, retained %d, dup %d, packetID %d\r\n", message.qos, message.retained, message.dup, message.id);
-    printf(msg);
-    ThisThread::sleep_for(1000ms);
-    char payload[300];
-    sprintf(payload, "Payload %.*s\r\n", message.payloadlen, (char*)message.payload);
-    printf(payload);
-    ++arrivedcount;
-    mode = 0;
-    printf("UI_gesture mode ends\n");
+  MQTT::Message &message = md.message;
+  char msg[300];
+  sprintf(msg, "Message arrived: QoS%d, retained %d, dup %d, packetID %d\r\n", message.qos, message.retained, message.dup, message.id);
+  printf(msg);
+  ThisThread::sleep_for(1000ms);
+  char payload[300];
+  sprintf(payload, "Payload %.*s\r\n", message.payloadlen, (char*)message.payload);
+  printf(payload);
+  ++arrivedcount;
+  mode = 0;
+  printf("UI_gesture mode ends\n");
 }
 
 void publish_message(MQTT::Client<MQTTNetwork, Countdown>* client) {
-    //message_num++;
-    if (mode == 1) {
-      mode = 0;
-      //printf("%d\n",mode);
+  if (mode == 1) {
+    mode = 0;
+    MQTT::Message message;
+    char buff[100];
+    sprintf(buff, "Select angle: %d", angle_select);
+    message.qos = MQTT::QOS0;
+    message.retained = false;
+    message.dup = false;
+    message.payload = (void*) buff;
+    message.payloadlen = strlen(buff) + 1;
+    int rc = client->publish(topic, message);
+    printf("%s\r\n", buff);
+  }
+  if (mode == 2 && (success_count < 10)) {
+    if (angle_value < cosine[array_index]) {
       MQTT::Message message;
       char buff[100];
-      sprintf(buff, "Select angle: %d", angle_select);
+      sprintf(buff, "tilt success #%d", (success_count + 1));
       message.qos = MQTT::QOS0;
       message.retained = false;
       message.dup = false;
@@ -90,29 +100,15 @@ void publish_message(MQTT::Client<MQTTNetwork, Countdown>* client) {
       message.payloadlen = strlen(buff) + 1;
       int rc = client->publish(topic, message);
       printf("%s\r\n", buff);
+      success_count++;
     }
-    if (mode == 2 && (success_count < 10)) {
-      printf("in mode 2");
-      if (angle_value < cosine[array_index]) {
-        MQTT::Message message;
-        char buff[100];
-        sprintf(buff, "tilt success #%d", (success_count + 1));
-        message.qos = MQTT::QOS0;
-        message.retained = false;
-        message.dup = false;
-        message.payload = (void*) buff;
-        message.payloadlen = strlen(buff) + 1;
-        int rc = client->publish(topic, message);
-        printf("%s\r\n", buff);
-        success_count++;
-      }
-    }
-    if (success_count >= 10) { 
-      mode = 0;
-      success_count = 0;
-    }
+  }
+  if (success_count >= 10) { 
+    mode = 0;
+    success_count = 0;
+    myled2 = 0;
+  }
 }
-
 
 // Return the result of the last prediction
 int PredictGesture(float* output) {
@@ -153,8 +149,6 @@ int PredictGesture(float* output) {
 
   return this_predict;
 }
-
-
 
 int main(int argc, char* argv[]) {
 
@@ -235,77 +229,74 @@ int main(int argc, char* argv[]) {
     }
 
 
-    printf("\nConnecting to %s...\r\n", MBED_CONF_APP_WIFI_SSID);
-    int ret = wifi->connect(MBED_CONF_APP_WIFI_SSID, MBED_CONF_APP_WIFI_PASSWORD, NSAPI_SECURITY_WPA_WPA2);
-    if (ret != 0) {
-            printf("\nConnection error: %d\r\n", ret);
-            return -1;
-    }
+  printf("\nConnecting to %s...\r\n", MBED_CONF_APP_WIFI_SSID);
+  int ret = wifi->connect(MBED_CONF_APP_WIFI_SSID, MBED_CONF_APP_WIFI_PASSWORD, NSAPI_SECURITY_WPA_WPA2);
+  if (ret != 0) {
+    printf("\nConnection error: %d\r\n", ret);
+      return -1;
+  }
 
 
-    NetworkInterface* net = wifi;
-    MQTTNetwork mqttNetwork(net);
-    MQTT::Client<MQTTNetwork, Countdown> client(mqttNetwork);
+  NetworkInterface* net = wifi;
+  MQTTNetwork mqttNetwork(net);
+  MQTT::Client<MQTTNetwork, Countdown> client(mqttNetwork);
 
-    //TODO: revise host to your IP
-    const char* host = "192.168.174.172";
-    printf("Connecting to TCP network...\r\n");
+  //TODO: revise host to your IP
+  const char* host = "192.168.174.172";
+  printf("Connecting to TCP network...\r\n");
 
-    SocketAddress sockAddr;
-    sockAddr.set_ip_address(host);
-    sockAddr.set_port(1883);
+  SocketAddress sockAddr;
+  sockAddr.set_ip_address(host);
+  sockAddr.set_port(1883);
 
-    printf("address is %s/%d\r\n", (sockAddr.get_ip_address() ? sockAddr.get_ip_address() : "None"),  (sockAddr.get_port() ? sockAddr.get_port() : 0) ); //check setting
-    int rc = mqttNetwork.connect(sockAddr);//(host, 1883);
-    if (rc != 0) {
-            printf("Connection error.");
-            return -1;
-    }
-    printf("Successfully connected!\r\n");
+  printf("address is %s/%d\r\n", (sockAddr.get_ip_address() ? sockAddr.get_ip_address() : "None"),  (sockAddr.get_port() ? sockAddr.get_port() : 0) ); //check setting
+  int rc = mqttNetwork.connect(sockAddr);//(host, 1883);
+  if (rc != 0) {
+    printf("Connection error.");
+    return -1;
+  }
+  printf("Successfully connected!\r\n");
 
-    MQTTPacket_connectData data = MQTTPacket_connectData_initializer;
-    data.MQTTVersion = 3;
-    data.clientID.cstring = "Mbed";
+  MQTTPacket_connectData data = MQTTPacket_connectData_initializer;
+  data.MQTTVersion = 3;
+  data.clientID.cstring = "Mbed";
 
-    if ((rc = client.connect(data)) != 0){
-            printf("Fail to connect MQTT\r\n");
-    }
-    if (client.subscribe(topic, MQTT::QOS0, messageArrived) != 0){
-            printf("Fail to subscribe\r\n");
-    }
+  if ((rc = client.connect(data)) != 0){
+    printf("Fail to connect MQTT\r\n");
+  }
+  if (client.subscribe(topic, MQTT::QOS0, messageArrived) != 0){
+    printf("Fail to subscribe\r\n");
+  }
 
-    mqtt_thread.start(callback(&mqtt_queue, &EventQueue::dispatch_forever));
-    mqtt_thread2.start(callback(&mqtt_queue2, &EventQueue::dispatch_forever));
-    btn.rise(mqtt_queue.event(&publish_message, &client));
+  mqtt_thread.start(callback(&mqtt_queue, &EventQueue::dispatch_forever));
+  mqtt_thread2.start(callback(&mqtt_queue2, &EventQueue::dispatch_forever));
+  btn.rise(mqtt_queue.event(&publish_message, &client));
 
   char buf[256], outbuf[256];
 
-    FILE *devin = fdopen(&pc, "r");
-    FILE *devout = fdopen(&pc, "w");
-    th.start(callback(&queue, &EventQueue::dispatch_forever));
-    th2.start(callback(&queue2, &EventQueue::dispatch_forever));
-    BSP_ACCELERO_Init();
+  FILE *devin = fdopen(&pc, "r");
+  FILE *devout = fdopen(&pc, "w");
+  th.start(callback(&queue, &EventQueue::dispatch_forever));
+  th2.start(callback(&queue2, &EventQueue::dispatch_forever));
+  BSP_ACCELERO_Init();
 
   while (true) {
-
     while(1) {
-      printf("in mode 0\n");
       memset(buf, 0, 256);
-        for (int i = 0; ; i++) {
-          char recv = fgetc(devin);
-          if (recv == '\n') {
-            printf("\r\n");
-              break;
-          }
-          buf[i] = fputc(recv, devout);
+      for (int i = 0; ; i++) {
+        char recv = fgetc(devin);
+        if (recv == '\n') {
+          printf("\r\n");
+            break;
         }
-        //Call the static call method on the RPC class
-        RPC::call(buf, outbuf);
-        printf("%s\r\n", outbuf);
-        if (mode != 0) break;
+        buf[i] = fputc(recv, devout);
+      }
+      //Call the static call method on the RPC class
+      RPC::call(buf, outbuf);
+      printf("%s\r\n", outbuf);
+      if (mode != 0) break;
     }
     while (mode == 1) {
-      //printf("in mode 1\n");
     // Attempt to read new data from the accelerometer
         got_data = ReadAccelerometer(error_reporter, model_input->data.f,
                                  input_length, should_clear_buffer);
@@ -332,34 +323,35 @@ int main(int argc, char* argv[]) {
 
     // Produce an output
         if (gesture_index < label_num) {
-        gesture_select = gesture_index;
-        error_reporter->Report(config.output_message[gesture_index]);
+          gesture_select = gesture_index;
+          error_reporter->Report(config.output_message[gesture_index]);
         }
       
     }
     while (mode == 2) {
       if (inital = 1) {
       BSP_ACCELERO_AccGetXYZ(value);
-      printf("%d %d %d\n", value[0], value[1], value[2]);
+      //printf("%d %d %d\n", value[0], value[1], value[2]);
       float temp1, temp2;
       temp1 = (float)((value[0]*initial_x + value[1]*initial_y + value[2]*initial_z));
       temp2 = (sqrt((double)(value[0]*value[0]+value[1]*value[1]+value[2]*value[2]))*sqrt((double)(initial_x*initial_x+initial_y*initial_y+initial_z*initial_z)));
-      printf("%.2f %.2f\n", temp1, temp2);
+      //printf("%.2f %.2f\n", temp1, temp2);
       angle_value = temp1 / temp2;
       printf("%.2f\n", angle_value);
       if (angle_value < cosine[array_index]) {
-        printf("Success\n");
         mqtt_queue2.call(&publish_message, &client);
-      }
+        myled3 = 1;
+      } else myled3 = 0;
       ThisThread::sleep_for(500ms);
       }
     }
   }
 }
-void UI_gesture (Arguments *in, Reply *out) {
+
+void UI_gesture (Arguments *in, Reply *out) 
+{
     queue.call(&gesture);
     bool success = true;
-
     char buffer[200], outbuf[256];
     char strings[25];
     sprintf(strings, "Start UI_gesture mode");
@@ -371,112 +363,91 @@ void UI_gesture (Arguments *in, Reply *out) {
         out->putData("Failed");
     }
 }
+
 void gesture() 
 {
-    mode = 1;
-    myled1 = 0;
-    for (int i = 0; i < 6; i++) {
-      myled1 = !myled1;
-      ThisThread::sleep_for(200ms);
+  mode = 1;
+  myled1 = 1;
+  uLCD.cls();
+  uLCD.printf("%d",angle[0]);
+  int test1 = 3;
+  int test2 = 3;
+  int test3 = 3;
+  while(1) {
+    if (gesture_select == 0 && (test1 != 1)) {
+      uLCD.cls();
+      uLCD.printf("Current angle: %d",angle[0]);
+      angle_select = angle[0];
+      array_index = 0;
+      test1 = 1;
+      test2 = 0;
+      test3 = 0;
+    } else if (gesture_select == 1 && (test2 != 1)) {
+      uLCD.cls();
+      uLCD.printf("Current angle: %d",angle[1]);
+      angle_select = angle[1];
+      array_index = 1;
+      test1 = 0;
+      test2 = 1;
+      test3 = 0;
+    } else if (gesture_select == 2 && (test3 != 1)) {
+      uLCD.cls();
+      uLCD.printf("Current angle: %d",angle[2]);
+      angle_select = angle[2];
+      array_index = 2;
+      test1 = 0;
+      test2 = 0;
+      test3 = 1;
     }
-    printf("%d\n", mode);
-    uLCD.cls();
-    uLCD.printf("%d",angle[0]);
-    while(1) {
-        if (gesture_select == 0) {
-            uLCD.cls();
-            uLCD.printf("Current angle: %d",angle[0]);
-            angle_select = angle[0];
-            array_index = 0;
-        } else if (gesture_select == 1) {
-            uLCD.cls();
-            uLCD.printf("Current angle: %d",angle[1]);
-            angle_select = angle[1];
-            array_index = 1;
-        } else if (gesture_select == 2) {
-            uLCD.cls();
-            uLCD.printf("Current angle: %d",angle[2]);
-            angle_select = angle[2];
-            array_index = 2;
-        }
-        if (mode == 0) {
-            break;
-        }
-    }   
+    if (mode == 0) {
+      myled1 = 0;
+      break;
+    }
+  }   
 }
-void angle_det(void) {
-    inital = 0;
-    mode = 2;
-    myled2 = 0;
-    int16_t pDataXYZ[3] = {0};
-    for (int i = 0; i < 6; i++) {
-      myled2 = !myled2;
-      ThisThread::sleep_for(200ms);
-    }
-    int num = 0;
-    int16_t x, y, z;
-    int16_t xtest = 0;
-    int16_t ytest = 0;
-    int16_t ztest = 0;
-    myled1 = 1;
-    while (num < 6) {
-      BSP_ACCELERO_AccGetXYZ(pDataXYZ);
-      x = pDataXYZ[0];
-      y = pDataXYZ[1];
-      z = pDataXYZ[2];
-       //printf("Accelerometer values: (%d, %d, %d)\n", pDataXYZ[0], pDataXYZ[1], pDataXYZ[2]);
-      if (x == xtest && y == ytest && z == ztest) {
-        num++;
-      }
-      xtest = x;
-      ytest = y;
-      ztest = z;
-    }
-    inital = 1;
-    initial_x = xtest;
-    initial_y = ytest;
-    initial_z = ztest;
-    printf("success initialize\n");
-    /*printf("%d %d %d\n", xtest, ytest, ztest);
-    printf("%d %d %d\n", value[0], value[1], value[2]);
-    myled1 = 0;
-    float angle_value;
-    float temp1, temp2;
-    temp1 = (float)((value[0]*xtest + value[1]*ytest + value[2]*ztest)^2);
-    temp2 = (float)((value[0]^2+value[1]^2+value[2]^2)*(xtest^2+ytest^2+ztest^2));
-    printf("%.2f %.2f\n", temp1, temp2);
-    angle_value = (float)((value[0]*xtest + value[1]*ytest + value[2]*ztest)^2) / (float)((value[0]^2+value[1]^2+value[2]^2)*(xtest^2+ytest^2+ztest^2));
-    printf("%.2f\n", angle_value);
-    int n = 0;*/
-    /*int n = 0;
-    while(n < 10) {
-      float angle_value;
-      float temp1, temp2;
-      temp1 = (float)((value[0]*xtest + value[1]*ytest + value[2]*ztest));
-      temp2 = (float)(sqrt(value[0]^2+value[1]^2+value[2]^2)*sqrt(xtest^2+ytest^2+ztest^2));
-      printf("%.2f %.2f\n", temp1, temp2);
-      angle_value = (float)((value[0]*xtest + value[1]*ytest + value[2]*ztest)) / (float)(sqrt(value[0]^2+value[1]^2+value[2]^2)*sqrt(xtest^2+ytest^2+ztest^2));
-      printf("%.2f\n", angle_value);
-      if (angle_value < cosine[array_index]) {
-        printf("tilt success\n");
-        ThisThread::sleep_for(500ms);
-        tilt_success = 1;
-        n++;
-      } else tilt_success = 0;
-    }*/
-}
-void angle_detection (Arguments *in, Reply *out) {
-    queue2.call(&angle_det);
-    bool success = true;
 
-    char buffer[200], outbuf[256];
-    char strings[25];
-    sprintf(strings, "Start angle_detection mode");
-    strcpy(buffer, strings);
-    //RPC::call(buffer, outbuf);
-    if (success) {
-        out->putData(buffer);
-    } else {
-        out->putData("Failed");
+void angle_det(void) 
+{
+  inital = 0;
+  mode = 2;
+  myled2 = 1;
+  int16_t pDataXYZ[3] = {0};
+  int num = 0;
+  int16_t x, y, z;
+  int16_t xtest = 0;
+  int16_t ytest = 0;
+  int16_t ztest = 0;
+  while (num < 6) {
+    BSP_ACCELERO_AccGetXYZ(pDataXYZ);
+    x = pDataXYZ[0];
+    y = pDataXYZ[1];
+    z = pDataXYZ[2];
+    if (x == xtest && y == ytest && z == ztest) {
+      num++;
     }
+    xtest = x;
+    ytest = y;
+    ztest = z;
+  }
+  inital = 1;
+  initial_x = xtest;
+  initial_y = ytest;
+  initial_z = ztest;
+  printf("Success initialize\n");
+}
+
+void angle_detection (Arguments *in, Reply *out) 
+{
+  queue2.call(&angle_det);
+  bool success = true;
+
+  char buffer[200], outbuf[256];
+  char strings[25];
+  sprintf(strings, "Start angle_detection mode");
+  strcpy(buffer, strings);
+  if (success) {
+    out->putData(buffer);
+  } else {
+    out->putData("Failed");
+  }
 }
